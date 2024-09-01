f18a01d688c64ceb9517db7ee578cbea2413669f
LukasKraiger <49635704+LukasKraiger@users.noreply.github.com>
2024-09-01 21:13:00 +0200

    Update R_source.sh

--- R_source.sh
+++ R_source.sh
@@ -4,13 +4,13 @@
 R_VERSION=4.2.2
 
 apt install -y g++ gcc gfortran libreadline-dev libx11-dev libxt-dev \
-            libpng-dev libjpeg-dev libcairo2-dev xvfb \
-            libbz2-dev libzstd-dev liblzma-dev libtiff5 \
-            libssh-dev libgit2-dev libcurl4-openssl-dev \
-            libblas-dev liblapack-dev libopenblas-base \
-            zlib1g-dev openjdk-17-jdk \
-            texinfo texlive texlive-fonts-extra \
-            screen wget libpcre2-dev make cmake
+    libpng-dev libjpeg-dev libcairo2-dev xvfb \
+    libbz2-dev libzstd-dev liblzma-dev libtiff-dev \
+    libssh-dev libgit2-dev libcurl4-openssl-dev \
+    libblas-dev liblapack-dev libopenblas-dev \
+    zlib1g-dev openjdk-17-jdk \
+    texinfo texlive texlive-fonts-extra \
+    screen wget libpcre2-dev make cmake
 cd /usr/local/src
 wget https://cran.rstudio.com/src/base/R-${R_VERSION:0:1}/R-$R_VERSION.tar.gz
 tar zxvf R-$R_VERSION.tar.gz
